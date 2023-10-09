c1f4e77d92d807910bf5d19168704405538671e8
ZidaneAhmed01 <zidaneahmed2007@gmail.com>
2023-10-09 19:16:07 +0000

    skill.js file

--- assets/js/BattleEvent.js
+++ assets/js/BattleEvent.js
@@ -1,6 +1,4 @@
 let Battle = false;
 let attack = false;
-let skill1 = false;
-let skill2 = false;
-let skill3 = false
-let ultimateskill = false 
+//initative decides if you or the enemy attacks first (true for player attacks first false for player attacks)
+let initiative = false 
---
+++ assets/js/Skills.js
@@ -0,0 +1,13 @@
+// hit and miss are the factor that decide if 
+let miss = false 
+let hit = false 
+//cooldowns for skill 2,3,4 and ultimate 
+let cooldown1 = 1
+let cooldown2 = 3
+let cooldown3 = 2
+let cooldown4 = 4 
+// variables for skills 2,3,4 and ultimate will be used for vfx and damage 
+let skill1
+let skill2
+let skill3
+let skill4 
