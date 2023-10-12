d748ac0cf729250a13d595807cdea6be30bf12ca
ZidaneAhmed01 <zidaneahmed2007@gmail.com>
2023-10-12 19:22:37 +0000

    a

--- assets/js/BattleEvent.js
+++ assets/js/BattleEvent.js
@@ -4,11 +4,10 @@ let npcAAlive = true;
 let heroAlive = true;
 let slimeAlive = true;
 //initative decides if you or the enemy attacks first (true for player attacks first false for player attacks)
-let initiative = false 
-function hitchance(max) {
-    return Math.floor(Math.random() * max);   
-}
-//wip
-function finitiative () {
-   hitchance(2)
+let initiative = 0 
+function initativechance(max) {
+    return Math.floor(Math.random() * max); 
+}  
+function BattlePreperation() {
+    initativechance(2)
 }
