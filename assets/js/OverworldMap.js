88de42f6896c271d42c4453d7e400fef47b37283
shaunli8 <shaunli008@icloud.com>
2023-10-25 12:19:37 -0700

    1

--- assets/js/OverworldMap.js
+++ assets/js/OverworldMap.js
@@ -495,6 +495,14 @@ window.OverworldMaps = {
         sizex: utils.withGrid(5), //length of the wall, if you set both sizex and sizey equal to 0, then the wall is just a point. 
         sizey: utils.withGrid(3), // width
       }),
+      wall15: new GameObject({
+        id: "wall15",   //name whatever you want, maybe like wa
+        wall: true,
+        x: utils.withGrid(65), // x-position of the wall 
+        y: utils.withGrid(15), // y-position of the wall 
+        sizex: utils.withGrid(2), //length of the wall, if you set both sizex and sizey equal to 0, then the wall is just a point. 
+        sizey: utils.withGrid(4), // width
+      }),
       walltop: new GameObject({
         id: "walltop",   //name whatever you want, maybe like wa
         wall: true,
