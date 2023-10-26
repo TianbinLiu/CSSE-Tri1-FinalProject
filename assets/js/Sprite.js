66cd0aeb555d04b72081382b8313e1831bd23870
shaunli8 <shaunli008@icloud.com>
2023-10-26 15:37:06 -0700

    1

--- assets/js/Sprite.js
+++ assets/js/Sprite.js
@@ -75,6 +75,12 @@ class Sprite {
       "slime3-walk-left": [[6, 6], [5, 6], [4, 6],[3, 6],[2, 6],[1, 6],],
       "slime3-attack-right": [[0, 2], [1, 2], [2, 2],[3, 2],[4, 2],[5, 2],[6, 2],],
       "slime3-attack-left": [[6, 7], [5, 7], [4, 7],[3, 7],[2, 7],[1, 7],[0, 7],],
+      "slime4-idle-right": [[0, 0], [1, 0], [2, 0],[3, 0],],
+      "slime4-idle-left": [[6, 5], [5, 5], [4, 5],[3, 5],],
+      "slime4-walk-right": [[0, 1], [1, 1], [2, 1],[3, 1],[4, 1],[5, 1],],
+      "slime4-walk-left": [[6, 6], [5, 6], [4, 6],[3, 6],[2, 6],[1, 6],],
+      "slime4-attack-right": [[0, 2], [1, 2], [2, 2],[3, 2],[4, 2],[5, 2],[6, 2],],
+      "slime4-attack-left": [[6, 7], [5, 7], [4, 7],[3, 7],[2, 7],[1, 7],[0, 7],],
       "marketplace-idle-right":[[0,0]],
     }
     this.currentAnimation = config.currentAnimation || (this.image.id + "-idle-right");
@@ -199,6 +205,18 @@ class Sprite {
       }
     }
 
+    if (this.image.id === "slime4") {
+      this.image.alive = slimeAlive;
+      if (!this.image.alive){
+        this.isBloodLoaded && ctx.drawImage(this.blood,
+          0, 0,
+          32, 32,
+          x, y,
+          this.image.sizex, this.image.sizey
+        );
+      }
+    }
+
     const [frameX, frameY] = this.frame;
 
     if (this.image.id === "hero") {
@@ -250,6 +268,18 @@ class Sprite {
     }
 
     if (this.image.id === "slime3") {
+      this.image.alive = slimeAlive;
+      if (this.image.alive) {
+        this.isLoaded && ctx.drawImage(this.image,
+          frameX * this.image.sizex, frameY * this.image.sizey,
+          this.image.sizex, this.image.sizey,
+          x, y,
+          this.image.sizex, this.image.sizey,
+        );
+      }
+    }
+
+    if (this.image.id === "slime4") {
       this.image.alive = slimeAlive;
       if (this.image.alive) {
         this.isLoaded && ctx.drawImage(this.image,
