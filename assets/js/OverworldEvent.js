f7c5be4f6ff3864c1f0d784b057f67fd60725f7b
ZidaneAhmed01 <zidaneahmed2007@gmail.com>
2023-10-26 19:39:54 -0700

    i dont know how to fix this

--- assets/js/OverworldEvent.js
+++ assets/js/OverworldEvent.js
@@ -37,6 +37,7 @@ class OverworldEvent {
       alive: this.alive,
       retry: true
     },)
+    
 
     //Set up a handler to complete when correct person is done walking, then resolve the event
     const completeHandler = e => {
@@ -47,7 +48,28 @@ class OverworldEvent {
     }
     document.addEventListener("PersonAttackingComplete", completeHandler)
   }
+  skill1(resolve) {
+    const who = this.map.gameObjects[this.event.who];
+    who.startBehavior({
+      map: this.map
+    }, {
+      type: "attackcrescent",
+      direction: this.event.direction,
+      spritedirection: this.event.spritedirection,
+      alive: this.alive,
+      retry: true
+    },)
+    
 
+    //Set up a handler to complete when correct person is done walking, then resolve the event
+    const completeHandler = e => {
+      if (e.detail.whoId === this.event.who) {
+        document.removeEventListener("PersonAttackingComplete", completeHandler);
+        resolve();
+      }
+    }
+    document.addEventListener("PersonAttackingComplete", completeHandler)
+  }
   walk(resolve) {
     const who = this.map.gameObjects[this.event.who];
     who.startBehavior({
